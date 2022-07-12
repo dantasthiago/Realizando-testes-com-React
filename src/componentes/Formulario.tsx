04a08c19612ed5f7e97138ea4362bd708441b505
dantasthiago <thiago_dantas88@hotmail.com>
2022-07-12 17:51:36 -0300

    criando componente de testes

---
+++ src/componentes/Formulario.tsx
@@ -0,0 +1,13 @@
+
+
+const Formulario = () => {
+  return(
+    <form>
+      <input type="text" placeholder="insira os nomes dos participantes"/>
+      <button disabled={true}>Adicionar</button>
+    </form>
+  )
+
+}
+
+export default Formulario
